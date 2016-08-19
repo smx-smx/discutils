61bbfcad9f85e135f764bf2c79c9a9521e31f7a1
smx-smx <smxdev4@gmail.com>
2016-08-19 18:52:12 +0200

    Set Code Policy to Visual Studio

--- src/BootConfig/Store.cs
+++ src/BootConfig/Store.cs
@@ -98,10 +98,10 @@ namespace DiscUtils.BootConfig
             return new BcdObject(_store, id);
         }
 
-		public BcdObject CreateObject(Guid id, ObjectType type)
-		{
-			return CreateObject (id, (int)type);
-		}
+        public BcdObject CreateObject(Guid id, ObjectType type)
+        {
+            return CreateObject (id, (int)type);
+        }
 
         /// <summary>
         /// Creates an application object.
